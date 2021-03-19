32e57e498aa2607b82deca1736ed2b17cafda155
xh <11675084@qq.com>
2021-03-19 15:34:07 +0800

    自动打开安装路径, 已测试window系统安卓apk

--- src/commands.js
+++ src/commands.js
@@ -1,7 +1,7 @@
 const vscode = require('vscode');
 const util = require('./util.js');
 const cli = require('./cli.js');
-
+const cp = require("child_process");
 
 
 
@@ -43,7 +43,20 @@ function buildApp(document) {
 					util.Tips(data)
 				} else if (code == -2 && data) {//进程正常返回数据
 					// 制作结果：Succeed.    安装包位置：E:/icpc_workspace_2/shougang/APP/zdhlAliyunApp/unpackage/release/apk/H5E14EBC2_20210319133334.apk
+					// 项目 zdhlAliyunApp [H5E14EBC2]的打包状态：时间: 2021-03-19 13:33:34    类型: Android自有证书    打包成功    安装包位置：E:/icpc_workspace_2/shougang/APP/zdhlAliyunApp/unpackage/release/apk/H5E14EBC2_20210319133334.apk
 					util.Output.appendLine(data); // 追加一行
+
+					if (data.indexOf("打包成功") != -1 && data.indexOf("安装包位置：") != -1) {
+						var appPath = data.split("安装包位置：")[1];
+						if (appPath) {
+							if (process.platform == "win32") {
+								cp.exec('explorer.exe /select,' + appPath.replace(/\//g, '\\'))
+							} else {
+								util.Output.appendLine("仅支持window自动打开目录,其他的我没有,谁赞助一哈 !!!∑(ﾟДﾟノ)ノ  ");
+							}
+						}
+
+					}
 				} else if (code == -3 && data) {//进程异常返回数据
 					util.Output.appendLine(data); // 追加一行
 				}
